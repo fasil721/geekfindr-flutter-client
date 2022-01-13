3c491dafe955aec2fb554bcb2fbbc87290a8bb7b
Fasil <fasil17062002@gmail.com>
2022-01-13 09:56:41 +0530

    intial commit

---
+++ android/app/src/main/kotlin/com/example/geek_findr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.geek_findr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:geek_findr/views/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
