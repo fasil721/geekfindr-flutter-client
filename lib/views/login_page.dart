f084198b1cd827509fc6f88103512763acc13893
Fasil <fasil17062002@gmail.com>
2022-01-13 13:38:05 +0530

    login page stack added

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -81,71 +81,107 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: size.width * 0.075,
-          right: size.width * 0.2,
-          bottom: size.height * 0.05,
-        ),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: size.height * 0.01),
-                child: Text(
-                  "Sign In",
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.bold,
-                    fontSize: size.width * .075,
-                  ),
-                ),
-              ),
-              SizedBox(height: size.height * 0.0175),
-              Container(
+      body: Stack(
+        clipBehavior: Clip.antiAlias,
+        children: [
+          Positioned(
+            bottom: size.height * .25,
+            child: RotationTransition(
+              turns: const AlwaysStoppedAnimation(-45 / 360),
+              child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: const Color(0xffE7EAF0),
-                ),
-                child: emailField,
-              ),
-              SizedBox(height: size.height * 0.0175),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: const Color(0xffE7EAF0),
-                ),
-                child: passwordField,
-              ),
-              SizedBox(height: size.height * 0.0175),
-              loginButton,
-              SizedBox(height: size.height * 0.0175),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: size.height * 0.005),
-                child: Text(
-                  "Or Sing up with social platform",
-                  style: GoogleFonts.roboto(
-                    fontSize: size.width * .036,
-                    fontWeight: FontWeight.w500,
+                  color: const Color(0xffB954FE),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(.7),
+                      blurRadius: 5,
+                      offset: const Offset(0, 7),
+                    ),
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(.7),
+                      blurRadius: 5,
+                      offset: const Offset(10, 0),
+                    )
+                  ],
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(1000),
                   ),
                 ),
+                height: size.height * .95,
+                width: size.width * 1.5,
               ),
-              SizedBox(height: size.height * 0.0175),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: size.height * 0.005),
-                  child: Image.asset(
-                    'assets/images/github.png',
-                    height: size.height * 0.04,
-                  ),
-                ),
-              )
-            ],
+            ),
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: size.width * 0.075,
+              right: size.width * 0.2,
+              bottom: size.height * 0.05,
+            ),
+            child: Form(
+              key: _formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: size.height * 0.01),
+                    child: Text(
+                      "Sign In",
+                      style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.bold,
+                        fontSize: size.width * .075,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.02),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xffE7EAF0),
+                    ),
+                    child: emailField,
+                  ),
+                  SizedBox(height: size.height * 0.02),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xffE7EAF0),
+                    ),
+                    child: passwordField,
+                  ),
+                  SizedBox(height: size.height * 0.02),
+                  loginButton,
+                  SizedBox(height: size.height * 0.02),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: size.height * 0.005),
+                    child: Text(
+                      "Or Sing up with social platform",
+                      style: GoogleFonts.roboto(
+                        fontSize: size.width * .036,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.02),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: size.height * 0.005),
+                      child: Image.asset(
+                        'assets/images/github.png',
+                        height: size.height * 0.04,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
