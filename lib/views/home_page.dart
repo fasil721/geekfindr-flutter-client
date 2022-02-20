356745a147313a5b030135552e5be2c034a6421d
Fasil <fasil17062002@gmail.com>
2022-02-20 12:39:29 +0530

    heart animating class created

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -72,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                                 _advancedDrawerController.showDrawer();
                               },
                             ),
-                          ),
+                          ), 
                           const SearchWidget()
                         ],
                       ),
